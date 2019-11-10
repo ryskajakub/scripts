2a2fd4dcd87dc871bdb21a8f8c953fd44d9aa0b4
Jakub Ryska <jakub.ryska@jumpshot.com>
2019-11-10 12:34:34 +0100

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@
 TARS=`ls -lah scripts-*.tgz | wc -l`
 
 if [ $TARS -eq 1 ] ; then
-  cp -R ~/debian/* scripts-1.0.0-1/
   mkdir -p scripts-1.0.0-1/var/www/scripts
+  cp -R debian/* scripts-1.0.0-1/
   tar -C scripts-1.0.0-1/var/www/scripts -xvzf scripts-*.tgz
 else
   echo "Didn't found one tgz to copy"
