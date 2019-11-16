68661fba869d19585479b7c6b985f3df81e20e32
Jakub Ryška <ryskajakub@seznam.cz>
2019-11-16 21:21:20 +0100

    reserve button

--- src/ParkingAvast.ml
+++ src/ParkingAvast.ml
@@ -61,15 +61,16 @@ let doReservation date (mode: mode): bool Js.Promise.t =
     let%bind passwordNext = FrameBase.waitForXPath page ~xpath:"//div[contains(@id, 'passwordNext')]" () in
     let%bind _ = waitPromise 10000 in
     let%bind _ = ElementHandle.click passwordNext () in
+    let%bind _ = waitPromise 20000 in
     let dtb = dayOfWeek in
     let buttonXPath = "//button[contains(@class, '" ^ firstButtonClass ^ "') and contains(., '" ^ dtb ^ "')]" in
-    let%bind reserveButton = FrameBase.waitForXPath page ~xpath:buttonXPath ~options:[%bs.obj { visible = Js.Nullable.undefined ; hidden = Js.Nullable.undefined ; timeout = Js.Nullable.return 60000.0 }] () in
+    let%bind reserveButton = FrameBase.waitForXPath page ~xpath:buttonXPath () in
     let%bind _ = ElementHandle.click reserveButton () in
     let%bind result = match mode with
       | MakeReservation ->
         let reservedButtonXPathText = "//button[contains(@class, '" ^ secondButtonClass ^ "')]/strong[contains(@class, 'inButtonText')]" in
-        let%bind reserveButton = FrameBase.waitForXPath page ~xpath:reservedButtonXPathText () in
-        let%bind textContent = ElementHandle.getProperty reserveButton ~propertyName:"textContent" in
+        let%bind reservedButton = FrameBase.waitForXPath page ~xpath:reservedButtonXPathText () in
+        let%bind textContent = ElementHandle.getProperty reservedButton ~propertyName:"textContent" in
         let%map jsonValue = ElementHandle.jsonValue textContent in
         let reservation = asString jsonValue in
         Some reservation, true 
@@ -92,4 +93,4 @@ let doReservation date (mode: mode): bool Js.Promise.t =
   
   promise |> Js.Promise.catch (fun e -> Js.log e ; Js.Promise.resolve false)
 
-let () = ignore @@ doReservation (Js.Date.make ()) MakeReservation
+(* let () = ignore @@ doReservation (Js.Date.make ()) MakeReservation *)
