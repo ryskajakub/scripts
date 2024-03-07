2790a92575325fe5318081d830689dffe8b728e5
Jakub Ryška <ryskajakub@seznam.cz>
2024-03-07 13:06:27 +0100

    master

--- scripts/push_to_dev.sh
+++ scripts/push_to_dev.sh
@@ -1,3 +1,4 @@
 BRANCH=`git branch --show-current`
+git push origin HEAD
 PULL_REQUEST_ID=`az repos pr create --source-branch $BRANCH --target-branch dev --delete-source-branch true --auto-complete | jq ".pullRequestId"`
 az repos pr set-vote --id $PULL_REQUEST_ID --vote approve
