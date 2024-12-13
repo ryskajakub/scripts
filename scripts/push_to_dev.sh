c3ddca8eb4146f5b799a6944942d7d3b334fb554
Jakub Ryška <ryskajakub@seznam.cz>
2024-12-13 14:02:43 +0100

    master

--- scripts/push_to_dev.sh
+++ scripts/push_to_dev.sh
@@ -4,6 +4,6 @@ git push origin HEAD
 PULL_REQUEST_ID=`az repos pr create --source-branch $BRANCH --target-branch $TARGET_BRANCH --squash --delete-source-branch true --auto-complete | jq ".pullRequestId"`
 az repos pr set-vote --id $PULL_REQUEST_ID --vote approve
 git checkout $TARGET_BRANCH
-until git pull | grep 'Already up to date.' | wc -l; do
-	echo "pulling"
+until git log -1 --oneline | grep get-settings | wc -l; do
+	git pull
 done
