2532a0c8d9839dbc59048cf2fbf02694c3e4ff65
Jakub Ryška <ryskajakub@seznam.cz>
2024-12-16 17:32:26 +0100

    master

--- scripts/push_to_dev.sh
+++ scripts/push_to_dev.sh
@@ -4,6 +4,6 @@ git push origin HEAD
 PULL_REQUEST_ID=`az repos pr create --source-branch $BRANCH --target-branch $TARGET_BRANCH --squash --delete-source-branch true --auto-complete | jq ".pullRequestId"`
 az repos pr set-vote --id $PULL_REQUEST_ID --vote approve
 git checkout $TARGET_BRANCH
-until git log -1 --oneline | grep " $BRANCH " | wc -l; do
+while result=$(git log -1 --oneline | grep " $BRANCH " | wc -l) && [ $result -eq 0 ] ; do
 	git pull
 done
